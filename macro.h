e47bd23ffcb46910893ad2add76b6b1c784c760c
NocturnalShadow <bogdanscloud@gmail.com>
2017-05-23 23:38:17 +0300

    Macros argument processing

--- macro.h
+++ macro.h
@@ -1,9 +1,10 @@
 #ifndef MACRO_H
 #define MACRO_H
 
+// Primitive concatenation
 #define CONCAT_(arg1, ...) arg1##__VA_ARGS__
 
-// increment
+// Increment
 #define INC(value) CONCAT_(INC_, value)
 #define INC_0 1
 #define INC_1 2
@@ -15,7 +16,7 @@
 #define INC_7 8
 #define INC_8 9
 
-// decrement
+// Decrement
 #define DEC(value) CONCAT_(DEC_, value)
 #define DEC_1 0
 #define DEC_2 1
@@ -27,79 +28,135 @@
 #define DEC_8 7
 #define DEC_9 8
 
-// multiple scans
+// Multiple preprocessor scans
 #define SCAN1(...)  __VA_ARGS__
 #define SCAN2(...)  SCAN1(SCAN1(SCAN1(__VA_ARGS__)))
 #define SCAN3(...)  SCAN2(SCAN2(SCAN2(__VA_ARGS__)))
 #define SCAN4(...)  SCAN3(SCAN3(SCAN3(__VA_ARGS__)))
 #define SCAN5(...)  SCAN4(SCAN4(SCAN4(__VA_ARGS__)))
-#define SCAN(...)   SCAN5(SCAN5(SCAN5(__VA_ARGS__)))
+#define SCAN6(...)  SCAN5(SCAN5(SCAN5(__VA_ARGS__)))
+#define SCAN7(...)  SCAN6(SCAN6(SCAN6(__VA_ARGS__)))
+#define SCAN8(...)  SCAN7(SCAN7(SCAN7(__VA_ARGS__)))
 
-// if and only if
+// Maximum scans
+#ifdef _MSC_VER
+    #define SCAN(...) SCAN6(__VA_ARGS__)
+#elif __MINGW32__ || __MINGW64__
+    #define SCAN(...) SCAN8(__VA_ARGS__)
+#endif
+
+// Defered execution
+#define EXEC(macro, ...) SCAN1(DEFER1(macro)(__VA_ARGS__))
+
+// Check 
+#ifdef _MSC_VER
+    #define CHECK(...) EXEC(ARG2, __VA_ARGS__, 0)
+    #define KEY 0, 1,
+#elif __MINGW32__
+    #define CHECK(...) ARG2(__VA_ARGS__, 0)
+    #define KEY 0, 1,
+#endif
+
+// Utility
+#define VOID()
+#define CONSUME(...)
+#define ECHO(value) value
+#define NOOP(...) (void) 0
+#define DEFER1(...) __VA_ARGS__ VOID()
+#define DEFER2(...) __VA_ARGS__ DEFER1(VOID)()
+
+// Get argument n
+#define ARG1(arg1, ...) arg1
+#define ARG2(arg1, arg2, ...) arg2
+
+// To String
+#define STRINGIFY(...) #__VA_ARGS__
+#define TOSTR(...) EXEC(STRINGIFY, __VA_ARGS__)
+
+// Get first argument if exists
+#define FIRST(...) EXEC(FIRST_, __VA_ARGS__,)
+#define FIRST_(first, ...) first
+// Get all argumets but the first
+#define REST(...) EXEC(REST_, __VA_ARGS__)
+#define REST_(first, ...) __VA_ARGS__
+
+// If an argument list is empty evaluated to 1 else 0
+#define ISEMPTY(...) EXEC(ISEMPTY_, FIRST(__VA_ARGS__))
+#define ISEMPTY_(...) CHECK(EMPTY##__VA_ARGS__)
+#define EMPTY KEY
+
+// If and only if (condition is 0 or 1 only)
 #define IIF(condition) CONCAT_(IIF_, condition)
 #define IIF_0(true_, ...) __VA_ARGS__
 #define IIF_1(true_, ...) true_
 
-// complement
+// Complement (value is 0 or 1 only)
 #define COMPL(value) CONCAT_(COMPL_, value)
 #define COMPL_0 1
 #define COMPL_1 0
 
-#define ARG2(unused, arg, ...) arg
-
-#define CHECK(...) ARG2(__VA_ARGS__, 0)
-#define KEY 0, 1,
-
-// not
-#define NOT(value) CHECK(CONCAT_(NOT_, value))
+// Not (1 if and only if value is 0 - else 0)
+#define NOT(value) CHECK(EXEC(CONCAT_, NOT_, value))
 #define NOT_0 KEY
 
-// bool
+// Bool (0 if and only if value is 0 - else 1)
 #define BOOL(value) COMPL(NOT(value))
 
-// if
+// If
 #define IF(condition) IIF(BOOL(condition))
 
-#define VOID()
-#define EAT(...)
-#define NOOP(...) (void) 0
-#define DEFER1(macro) macro VOID()
-#define OBSTRUCT(...) __VA_ARGS__ DEFER1(VOID)()
+// When
+#define WHEN(condition) IF(condition)(SCAN1, CONSUME)
 
-// when
-#define WHEN(condition) IF(condition)(SCAN1, NOOP)
-
-// repeate
-#define REPEAT(count, macro, ...) \
+// WARNING: unteseted!
+// Repeat loop
+#define REPEAT(count, _operator, ...) \
     WHEN(count) \
     ( \
-        OBSTRUCT(REPEAT_INDIRECT)() ( \
-            DEC(count), macro, __VA_ARGS__ \
+        DEFER2(REPEAT_INDIRECT)() ( \
+            DEC(count), _operator, __VA_ARGS__ \
         ) \
-        OBSTRUCT(macro) ( \
+        DEFER1(_operator) ( \
             DEC(count), __VA_ARGS__ \
         ) \
     )
 #define REPEAT_INDIRECT() REPEAT
 
-// for
-#define FOR(count, macro)\
+// WARNING: unteseted!
+// For loop
+#define FOR(count, _operator) \
     WHEN(count) \
     ( \
-        OBSTRUCT(FOR_INDIRECT)() ( \
-            DEC(count), macro \
+        DEFER2(FOR_INDIRECT)() ( \
+            DEC(count), _operator \
         ) \
-        OBSTRUCT(macro) ( \
+        DEFER1(_operator) ( \
             DEC(count) \
         ) \
     )
 #define FOR_INDIRECT() FOR
 
-// argument count
+// WARNING: unteseted!
+// While loop
+#define WHILE(predicate, _operator, ...) \
+    IF(pred(__VA_ARGS__)) \
+    ( \
+        DEFER2(WHILE_INDIREDT) () ( \
+            predicate, _operator, _operator(__VA_ARGS__) \
+        ), \
+        __VA_ARGS__ \
+    )
+#define WHILE_INDIREDCT() WHILE
+
+// "Remove coma if no args" trick
+#define FIX_COMA_AFTER(...) \
+    IF(ISEMPTY(__VA_ARGS__)) (, __VA_ARGS__,)
+
+// Argument count
 #define COUNT(...) \
-         COUNT_(__VA_ARGS__, ARG_NUMBERS())
-#define COUNT_(...) \
-         ARG_N(__VA_ARGS__)
+         COUNT_NOT_0(FIX_COMA_AFTER(__VA_ARGS__) ARG_NUMBERS())
+#define COUNT_NOT_0(...) \
+         EXEC(ARG_N, __VA_ARGS__)
 #define ARG_N( \
           _1, _2, _3, _4, _5, _6, _7, _8, _9,_10, \
          _11,_12,_13,_14,_15,_16,_17,_18,_19,_20, \
@@ -109,7 +166,7 @@
          _51,_52,_53,_54,_55,_56,_57,_58,_59,_60, \
          _61,_62,_63, N,...) N
 #define ARG_NUMBERS() \
-         63,62,61,60,                   \
+         63,62,61,60, \
          59,58,57,56,55,54,53,52,51,50, \
          49,48,47,46,45,44,43,42,41,40, \
          39,38,37,36,35,34,33,32,31,30, \
@@ -117,5 +174,14 @@
          19,18,17,16,15,14,13,12,11,10, \
          9,8,7,6,5,4,3,2,1,0
 
+// Applies consequently a macro to each subsequent argument
+#define FOR_EACH(macro, ...) SCAN(FOR_EACH_(macro, __VA_ARGS__))
+#define FOR_EACH_(macro, ...) \
+    WHEN(NOT(ISEMPTY(__VA_ARGS__))) \
+    ( \
+        EXEC(macro, FIRST(__VA_ARGS__)) \
+        DEFER2(FOR_EACH_INDIRECT) () (macro, REST(__VA_ARGS__)) \
+    )
+#define FOR_EACH_INDIRECT() FOR_EACH_
 
 #endif // MACRO_H
--- main.cpp
+++ main.cpp
@@ -1,25 +1,42 @@
 #include <QCoreApplication>
 #include <QDebug>
 
-#define VOID()
-#define EXPAND(...) __VA_ARGS__
-#define DEFER(macro) macro VOID()
-#define STRINGIFY(...) #__VA_ARGS__
+#include "macro.h"
 
-#define TOSTR(...) EXPAND(DEFER(STRINGIFY)(__VA_ARGS__))
-
-#define PROBE(x) x, 1,
-#define xxx 1, 2
-
-#define func(a, b) a + b
+#define FUNCTION(name) void name(int i);
 
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    qDebug() << TOSTR(xxx);
-    qDebug() << EXPAND(DEFER(func)(xxx));
+    qDebug() << TOSTR(FIRST(1, 2, 3))   << "SHOULD BE 1";
+    qDebug() << TOSTR(FIRST()0)         << "SHOULD BE 0";
+
+    qDebug() << TOSTR(REST(1, 2))       << "SHOULD BE 2";
+    qDebug() << TOSTR(REST(1)0)         << "SHOULD BE 0";
+
+    qDebug() << TOSTR(ISEMPTY_())       << "SHOULD BE 1";
+    qDebug() << TOSTR(ISEMPTY_(1))      << "SHOULD BE 0";
+
+    qDebug() << ISEMPTY()               << "SHOULD BE 1";
+    qDebug() << ISEMPTY(1, 4, 3)        << "SHOULD BE 0";
+
+    qDebug() << NOT(ISEMPTY(arg1, arg2, arg3)) << "SHOULD BE 1";
+
+    qDebug() << WHEN(NOT(ISEMPTY(arg1, arg2, arg3))) ( TOSTR(Correct) );
+    qDebug() << WHEN(NOT(ISEMPTY())) ( TOSTR(Incorrect) ) "Correct";
+
+    qDebug() << TOSTR(FOR_EACH(ECHO, arg1, arg2, arg3)) << "SHOULD BE arg1, arg2, arg3";
+    qDebug() << TOSTR(FOR_EACH(ECHO, arg1)) << "SHOULD BE arg1";
+
+    qDebug() << COUNT()                 << "SHOULD BE 0";
+    qDebug() << COUNT(arg1)             << "SHOULD BE 1";
+    qDebug() << COUNT(arg1, arg2)       << "SHOULD BE 2";
+
+    qDebug() << TOSTR(FOR_EACH(FUNCTION, func1)) << "SHOULD BE void func1();";
+
+    qDebug() << TOSTR(FOR_EACH(FUNCTION, func1, func2, func3, func4, func5, func6));
 
     return a.exec();
 }
